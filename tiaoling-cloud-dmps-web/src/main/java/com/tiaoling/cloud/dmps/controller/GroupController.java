f30ef8ea200581a292b43979d9c41d80568c24b3
yuhonglie <yuhonglie@xiaojiuwo.cn>
2016-10-11 14:39:36 +0800

    add dmps

---
+++ tiaoling-cloud-dmps-web/src/main/java/com/tiaoling/cloud/dmps/controller/GroupController.java
@@ -0,0 +1,158 @@
+package com.tiaoling.cloud.dmps.controller;
+
+import com.tiaoling.cloud.dmps.domain.ConfGroup;
+import com.tiaoling.cloud.dmps.utils.CommonPropertiesUtils;
+import com.tiaoling.cloud.dmps.utils.HttpUtils;
+import com.tiaoling.cloud.dmps.utils.JsonUtils;
+import com.tiaoling.cloud.dmps.utils.ReturnT;
+import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by yhl on 2016/10/10.
+ */
+@Controller
+@RequestMapping(value = "/group")
+public class GroupController {
+    @RequestMapping
+    public String index(Model model) {
+        List<ConfGroup> list = new ArrayList<>();
+        String json ="";
+        Map<String,Object> params = new HashMap<String,Object>();
+        String doMethod = CommonPropertiesUtils.get("group_list");
+        json = HttpUtils.doPost(CommonPropertiesUtils.get("cloud_conf_api_url") + "/"
+                + doMethod, JsonUtils.getJSONString(params));
+        if(StringUtils.isNotBlank(json))
+        {
+            Map<String,Object> map = JsonUtils.toMap(json);
+            if(map.get("Code").toString().equals("1") && map.get("Body")!=null)
+            {
+                list=JsonUtils.getListDTO(JsonUtils.getJSONString(map.get("Body")),ConfGroup.class);
+            }
+        }
+        model.addAttribute("list", list);
+        return "group/group.index";
+    }
+    @RequestMapping("/save")
+    @ResponseBody
+    public ReturnT<String> save(ConfGroup ConfGroup){
+        // valid
+        if (ConfGroup.getGroupKey()==null || StringUtils.isBlank(ConfGroup.getGroupKey())) {
+            return new ReturnT<String>(500, "请输入GroupKey");
+        }
+        if (ConfGroup.getGroupKey().length()<1 || ConfGroup.getGroupKey().length()>100) {
+            return new ReturnT<String>(500, "GroupKey长度限制为1~100");
+        }
+        if (ConfGroup.getGroupName()==null || StringUtils.isBlank(ConfGroup.getGroupName())) {
+            return new ReturnT<String>(500, "请输入分组名称");
+        }
+        if (ConfGroup.getGroupName().length()<1 || ConfGroup.getGroupName().length()>100) {
+            return new ReturnT<String>(500, "GroupName长度限制为1~100");
+        }
+        int ret =0;
+        String json ="";
+        String message="";
+        Map<String,Object> params = new HashMap<String,Object>();
+        params.put("groupKey",ConfGroup.getGroupKey());
+        params.put("groupName",ConfGroup.getGroupName());
+        String doMethod = CommonPropertiesUtils.get("group_add");
+        json = HttpUtils.doPost(CommonPropertiesUtils.get("cloud_conf_api_url") + "/"
+                + doMethod, JsonUtils.getJSONString(params));
+        if(StringUtils.isNotBlank(json))
+        {
+            Map<String,Object> map = JsonUtils.toMap(json);
+            if(map.get("Code").toString().equals("1") && map.get("Body")!=null)
+            {
+                JSONObject object = JSONObject.fromObject(map.get("Body").toString());
+                ret = object.getInt("row");
+            }
+            else
+            {
+                message=map.get("Message").toString();
+            }
+        }
+        return (ret>0)?ReturnT.SUCCESS:ReturnT.FAIL;
+    }
+    @RequestMapping("/update")
+    @ResponseBody
+    public ReturnT<String> update(ConfGroup ConfGroup){
+
+        // valid
+        if (ConfGroup.getGroupKey()==null || StringUtils.isBlank(ConfGroup.getGroupKey())) {
+            return new ReturnT<String>(500, "请输入GroupKey");
+        }
+        if (ConfGroup.getGroupKey().length()<1 || ConfGroup.getGroupKey().length()>100) {
+            return new ReturnT<String>(500, "GroupKey长度限制为1~100");
+        }
+        if (ConfGroup.getGroupName()==null || StringUtils.isBlank(ConfGroup.getGroupName())) {
+            return new ReturnT<String>(500, "请输入分组名称");
+        }
+        if (ConfGroup.getGroupName().length()<1 || ConfGroup.getGroupName().length()>100) {
+            return new ReturnT<String>(500, "GroupName长度限制为1~100");
+        }
+
+        int ret =0;
+        String json ="";
+        String message="";
+        Map<String,Object> params = new HashMap<String,Object>();
+        params.put("groupKey",ConfGroup.getGroupKey());
+        params.put("groupName",ConfGroup.getGroupName());
+        params.put("isRemove",0);
+        String doMethod = CommonPropertiesUtils.get("group_update");
+        json = HttpUtils.doPost(CommonPropertiesUtils.get("cloud_conf_api_url") + "/"
+                + doMethod, JsonUtils.getJSONString(params));
+        if(StringUtils.isNotBlank(json))
+        {
+            Map<String,Object> map = JsonUtils.toMap(json);
+            if(map.get("Code").toString().equals("1") && map.get("Body")!=null)
+            {
+                JSONObject object = JSONObject.fromObject(map.get("Body").toString());
+                ret = object.getInt("row");
+            }
+            else
+            {
+                message=map.get("Message").toString();
+            }
+        }
+        return (ret>0)?ReturnT.SUCCESS:new ReturnT<String>(500, message);
+    }
+    @RequestMapping("/remove")
+    @ResponseBody
+    public ReturnT<String> remove(String groupKey){
+
+        int ret =0;
+        String json ="";
+        String message="";
+        Map<String,Object> params = new HashMap<String,Object>();
+        params.put("groupKey",groupKey);
+        params.put("isRemove",1);
+        params.put("isdelete",1);
+        String doMethod = CommonPropertiesUtils.get("group_update");
+        json = HttpUtils.doPost(CommonPropertiesUtils.get("cloud_conf_api_url") + "/"
+                + doMethod, JsonUtils.getJSONString(params));
+        if(StringUtils.isNotBlank(json))
+        {
+            Map<String,Object> map = JsonUtils.toMap(json);
+            if(map.get("Code").toString().equals("1") && map.get("Body")!=null)
+            {
+                JSONObject object = JSONObject.fromObject(map.get("Body").toString());
+                ret = object.getInt("row");
+            }
+            else
+            {
+                message=map.get("Message").toString();
+            }
+        }
+        return (ret>0)?ReturnT.SUCCESS:new ReturnT<String>(500, message);
+    }
+
+}
---
+++ tiaoling-cloud-dmps-web/src/main/webapp/static/js/group.index.1.js
@@ -0,0 +1,174 @@
+$(function() {
+
+	// remove
+	$('.remove').on('click', function(){
+		var groupKey = $(this).attr('groupKey');
+
+		ComConfirm.show("确认删除分组?", function(){
+			$.ajax({
+				type : 'POST',
+				url : base_url + '/group/remove',
+				data : {"groupKey":groupKey},
+				dataType : "json",
+				success : function(data){
+					if (data.code == 200) {
+						ComAlert.show(1, '删除成功', function () {
+							window.location.reload();
+						});
+					} else {
+						if (data.msg) {
+							ComAlert.show(2, data.msg);
+						} else {
+							ComAlert.show(2, '删除失败');
+						}
+					}
+				},
+			});
+		});
+	});
+
+	// jquery.validate 自定义校验 “英文字母开头，只含有英文字母、数字和下划线”
+	jQuery.validator.addMethod("myValid01", function(value, element) {
+		var length = value.length;
+		var valid = /^[a-z][a-zA-Z0-9-]*$/;
+		return this.optional(element) || valid.test(value);
+	}, "限制以小写字母开头，由小写字母、数字和中划线组成");
+
+	$('.add').on('click', function(){
+		$('#addModal').modal({backdrop: false, keyboard: false}).modal('show');
+	});
+	var addModalValidate = $("#addModal .form").validate({
+		errorElement : 'span',
+		errorClass : 'help-block',
+		focusInvalid : true,
+		rules : {
+			groupKey : {
+				required : true,
+				rangelength:[1,100],
+				myValid01 : true
+			},
+			groupName : {
+				required : true,
+				rangelength:[1, 100]
+			}
+		},
+		messages : {
+			groupKey : {
+				required :"请输入“GroupKey”",
+				rangelength:"GroupKey长度1~100",
+				myValid01: "限制以小写字母开头，由小写字母、数字和中划线组成"
+			},
+			groupName : {
+				required :"请输入“分组名”",
+				rangelength:"长度限制为1~100"
+			},
+			order : {
+				required :"请输入“排序”",
+				digits: "请输入整数",
+				range: "取值范围为1~1000"
+			}
+		},
+		highlight : function(element) {
+			$(element).closest('.form-group').addClass('has-error');
+		},
+		success : function(label) {
+			label.closest('.form-group').removeClass('has-error');
+			label.remove();
+		},
+		errorPlacement : function(error, element) {
+			element.parent('div').append(error);
+		},
+		submitHandler : function(form) {
+			$.post(base_url + "/group/save",  $("#addModal .form").serialize(), function(data, status) {
+				if (data.code == "200") {
+					$('#addModal').modal('hide');
+					setTimeout(function () {
+						ComAlert.show(1, "新增成功", function(){
+							window.location.reload();
+						});
+					}, 315);
+				} else {
+					if (data.msg) {
+						ComAlert.show(2, data.msg);
+					} else {
+						ComAlert.show(2, "新增失败");
+					}
+				}
+			});
+		}
+	});
+	$("#addModal").on('hide.bs.modal', function () {
+		$("#addModal .form")[0].reset();
+		addModalValidate.resetForm();
+		$("#addModal .form .form-group").removeClass("has-error");
+	});
+
+	$('.update').on('click', function(){
+		$("#updateModal .form input[name='groupKey']").val($(this).attr("groupKey"));
+		$("#updateModal .form input[name='groupName']").val($(this).attr("groupName"));
+
+		$('#updateModal').modal({backdrop: false, keyboard: false}).modal('show');
+	});
+	var updateModalValidate = $("#updateModal .form").validate({
+		errorElement : 'span',
+		errorClass : 'help-block',
+		focusInvalid : true,
+		rules : {
+			groupKey : {
+				required : true,
+				rangelength:[1,100],
+				myValid01 : true
+			},
+			groupName : {
+				required : true,
+				rangelength:[1, 100]
+			}
+		},
+		messages : {
+			groupKey : {
+				required :"请输入“GroupKey”",
+				rangelength:"GroupKey长度限制为1~100",
+				myValid01: "限制以小写字母开头，由小写字母、数字和中划线组成"
+			},
+			groupName : {
+				required :"请输入“分组名”",
+				rangelength:"长度限制为1~100"
+			}
+		},
+		highlight : function(element) {
+			$(element).closest('.form-group').addClass('has-error');
+		},
+		success : function(label) {
+			label.closest('.form-group').removeClass('has-error');
+			label.remove();
+		},
+		errorPlacement : function(error, element) {
+			element.parent('div').append(error);
+		},
+		submitHandler : function(form) {
+			$.post(base_url + "/group/update",  $("#updateModal .form").serialize(), function(data, status) {
+				if (data.code == "200") {
+					$('#addModal').modal('hide');
+					setTimeout(function () {
+						ComAlert.show(1, "更新成功", function(){
+							window.location.reload();
+						});
+					}, 315);
+				} else {
+					if (data.msg) {
+						ComAlert.show(2, data.msg);
+					} else {
+						ComAlert.show(2, "更新失败");
+					}
+				}
+			});
+		}
+	});
+	$("#updateModal").on('hide.bs.modal', function () {
+		$("#updateModal .form")[0].reset();
+		addModalValidate.resetForm();
+		$("#updateModal .form .form-group").removeClass("has-error");
+	});
+
+	
+});
